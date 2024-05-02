f730b9fc09c914283e7864575f76f8cc20301cd0
Engin Bolat <engi.bolat@gmail.com>
2024-05-02 20:59:58 +0300

    created PortfolioCard

---
+++ src/app/components/card/product-card/layout.tsx
@@ -0,0 +1,24 @@
+const PortfolioCard: React.FC<{
+    path: string;
+    title: string;
+    link: string;
+}> = ({
+    path,
+    title,
+    link,
+}) => {
+        return <a href={link} className="cursor-pointer">
+            <div className="relative bg-transparent group rounded-xl border border-gray-200 cursor-pointer">
+                <img
+                    src={path}
+                    className=' rounded-xl object-contain cursor-pointer'
+                />
+                <div className="cursor-pointer rounded-l-lg rounded-br-xl absolute right-0 bottom-0 p-2 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-lg bg-zinc-500 bg-opacity-30">
+                    <span>{title}</span>
+                </div>
+                <div className="absolute inset-0 cursor-pointer" />
+            </div>
+        </a>
+    }
+
+export default PortfolioCard;
