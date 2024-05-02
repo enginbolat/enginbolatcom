4a2b2e44feae609ce1a771339124a4bb060f8bdb
Engin Bolat <engi.bolat@gmail.com>
2024-05-02 21:00:51 +0300

    added techstack

---
+++ src/app/data/data.ts
@@ -0,0 +1,155 @@
+import { ProjectModel, SocialMediaModel, TechStack } from "../model";
+
+export const projects: ProjectModel[] = [
+  {
+    "id": 0,
+    "name": "Antalya Kart",
+    "link": "https://github.com/EnginBolat/antalya_kart_redesign",
+    "image": "/images/ant-kart.jpg",
+  },
+  {
+    "id": 1,
+    "name": "Antalya Sektörel",
+    "link": "https://apps.apple.com/us/app/antalya-sekt%C3%B6rel/id6451334484",
+    "image": "/images/ant-sektorel.jpg",
+  },
+  {
+    "id": 2,
+    "name": "Meal App",
+    "link": "https://github.com/EnginBolat/meal-app-bloc",
+    "image": "/images/meal-app.jpg",
+  },
+  {
+    "id": 3,
+    "name": "Metaggio",
+    "link": "https://metaggio.com/",
+    "image": "/images/metaggio.jpg",
+  }
+]
+
+
+export const socialMedia: SocialMediaModel[] = [
+  {
+    id: 0,
+    path: "",
+    link: "/instagram",
+  },
+  {
+    id: 1,
+    path: "",
+    link: "/twitter",
+  },
+  {
+    id: 2,
+    path: "",
+    link: "/youtube",
+  },
+  {
+    id: 3,
+    path: "",
+    link: "/github",
+  },
+  {
+    id: 4,
+    path: "",
+    link: "/linkedin",
+  },
+  {
+    id: 5,
+    path: "",
+    link: "/spotify"
+  },
+  {
+    id: 6,
+    path: "",
+    link: "/medium"
+  }
+];
+
+export const techStack: TechStack[] = [
+  {
+    id: 0,
+    name: "React Native",
+    imagePath: "./ic/ic_react.png"
+  },
+  {
+    id: 1,
+    name: "Swift",
+    imagePath: "./ic/ic_swift.png"
+  },
+  {
+    id: 2,
+    name: "Flutter",
+    imagePath: "./ic/ic_flutter.png"
+  },
+  {
+    id: 3,
+    name: "NextJS",
+    imagePath: "./ic/ic_next.png"
+  },
+  {
+    id: 4,
+    name: "Redux",
+    imagePath: "./ic/ic_redux.png"
+  },
+  {
+    id: 5,
+    name: "Tailwind/Nativewind",
+    imagePath: "./ic/ic_tailwind.png"
+  },
+  {
+    id: 6,
+    name: "MYSQL",
+    imagePath: "./ic/ic_mysql.png"
+  },
+  {
+    id: 7,
+    name: "Javascript",
+    imagePath: "./ic/ic_javascript.png"
+  },
+  {
+    id: 8,
+    name: "Typescript",
+    imagePath: "./ic/ic_typescript.png"
+  },
+  {
+    id: 9,
+    name: "Jest",
+    imagePath: "./ic/ic_jest.png"
+  },
+  {
+    id: 10,
+    name: ".NET Core",
+    imagePath: "./ic/ic_net.png"
+  },
+  {
+    id: 11,
+    name: "pnpm",
+    imagePath: "./ic/ic_pnpm.png"
+  },
+  {
+    id: 12,
+    name: "npm",
+    imagePath: "./ic/ic_npm.png"
+  },
+  {
+    id: 13,
+    name: "GIT",
+    imagePath: "./ic/ic_git.png"
+  },
+  {
+    id: 14,
+    name: "Figma",
+    imagePath: "./ic/ic_figma.png"
+  },
+  {
+    id: 15,
+    name: "HTML",
+    imagePath: "./ic/ic_html.png"
+  },
+  {
+    id: 16,
+    name: "CSS",
+    imagePath: "./ic/ic_css.png"
+  },
+]
