1f46c22d10847bc86234a2daa2e21499f8172982
Engin Bolat <engi.bolat@gmail.com>
2024-05-03 16:30:40 +0300

    title font type changed as bold

--- src/app/components/title/section-title/layout.tsx
+++ src/app/components/title/section-title/layout.tsx
@@ -2,11 +2,7 @@ import React from "react"
 
 export default function SectionTitlt({ title, seeMore = false, navigationUrl }: { title: String, seeMore?: boolean, navigationUrl?: string }) {
     return <div className="flex justify-between items-center text-end flex-row">
-        <span className="text-black text-3xl py-5 font-medium">{title}</span>
-        {seeMore &&
-            <a href={navigationUrl}><span className="text-gray-300 text-lg ">
-                Read More</span>
-            </a>
-        }
+        <span className="text-black text-3xl py-5 font-bold">{title}</span>
+        {seeMore && <a href={navigationUrl}><span className="text-gray-300 text-lg ">Read More</span></a>}
     </div>
 }
