2cd428819408e1551bdfc7b28824d82d19dd6021
Engin Bolat <engi.bolat@gmail.com>
2024-05-03 16:29:58 +0300

    inter font changed as poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import "./techstack.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight:['100','200','300','400','500','600','700','800','900'],
+});
 
 export const metadata: Metadata = {
   title: "Engin Bolat",
