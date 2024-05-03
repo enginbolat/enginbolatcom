2903770b81486fd3acc3674d0e3af51cde71b842
Engin Bolat <engi.bolat@gmail.com>
2024-05-03 16:35:07 +0300

    circle button width changed as p-12 for the mobile

--- src/app/components/button/circlebutton/layout.tsx
+++ src/app/components/button/circlebutton/layout.tsx
@@ -3,7 +3,7 @@ const CirlceButton: React.FC<{
     path: string,
 }> = ({ link, path }) => {
     return <a href={link} className="block" target='_blank' rel='noopener noreferrer' >
-        <img className="block rounded-xl hover:bg-gray-100 1 max-md:w-12 max-md:h-12 max-lg:w-16 max-lg:h-16 p-2" src={path}/>
+        <img className="block rounded-xl hover:bg-gray-100 1 max-md:w-16 max-md:h-12 max-lg:w-16 max-lg:h-16 p-2" src={path}/>
     </a>
 }
 
