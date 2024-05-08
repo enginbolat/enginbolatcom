ce8498bb634db2f58249959f68b5ead5446d7296
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 17:45:14 +0300

    added exports for the education and experience

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -1,3 +1,5 @@
 export { projects as ProjectData } from './data';
 export { socialMedia as SocialMediaData } from './data';
 export { techStack as TechStack } from './data';
+export { education as Education } from './data';
+export { experiences as Experiences } from './data';
