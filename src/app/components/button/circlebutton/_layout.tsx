07bfaa1141692881ad5476825448b9921765d436
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 18:36:00 +0300

    changed mid screen icons width in circle button

--- src/app/components/button/circlebutton/_layout.tsx
+++ src/app/components/button/circlebutton/_layout.tsx
@@ -8,7 +8,7 @@ interface CircleButtonProps {
 export function CircleButton({ link, path }: CircleButtonProps) {
     return (
         <Link href={link} className="block transation duration-300 transform hover:scale-110 hover:zoom-in hover:ease-in-out hover:-rotate-3" target="_blank">
-            <img className="block rounded-xl hover:bg-gray-100 max-md:w-16 max-md:h-11 max-lg:w-12 max-lg:h-12 p-2" src={path} />
+            <img className="block rounded-xl hover:bg-gray-100 max-md:w-16 max-md:h-16 max-lg:w-14 max-lg:h-14 p-2" src={path} />
         </Link>
     );
 }
