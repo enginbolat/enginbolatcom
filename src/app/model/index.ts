515cdcff268a7fe83d579e4bb4c61dca780dbf29
Engin Bolat <engi.bolat@gmail.com>
2024-06-01 00:06:54 +0300

    Added Experience model export

--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -1,3 +1,5 @@
+
 export type { Project as ProjectModel } from './project.model';
+export type { TechStack as TechStack } from './tech.stack.model';
+export type { Experience as Experience } from './experience.model';
 export type { SocialMedia as SocialMediaModel } from './social.media.model'
-export type {TechStack as TechStack} from './tech.stack.model';
